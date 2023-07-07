e026d0ba8c2d3deec85b982801c1e75c2848318e
SkywalkerDarth <danteuche@gmail.com>
2023-07-07 03:42:24 +0100

    All tasks

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+""" annotated function taking two strings and concatenating them """
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """
+    function concat to concatenate str1 and str2
+
+    Args:
+        str1: 1st string
+        str2: 2nd string
+
+    Return:
+        concat
+    """
+
+    return str1 + str2
