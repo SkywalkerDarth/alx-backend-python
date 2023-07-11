c404ebb6ad184dc28ffe0e2d20a7b194fcff0586
SkywalkerDarth <danteuche@gmail.com>
2023-07-11 13:49:05 +0100

    Generator error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ function generate rsndom numbers asynchron
     Args: none
     Return:
